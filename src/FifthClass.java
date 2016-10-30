c048ef75edca8526e361ba3d69ebb7c615ab1099
Andriy Hafyak <x3x7x3@gmai.com>
2016-10-30 11:35:54 +0100

    Homework)))

---
+++ src/FifthClass.java
@@ -0,0 +1,12 @@
+/**
+ * Created by user on 30.10.2016.
+ */
+public class FifthClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Мой первый класс на Java");
+
+
+    }
+}
---
+++ src/ThirdClass.java
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 30.10.2016.
+ */
+public class ThirdClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Мой первый класс на Java");
+
+    }
+}
