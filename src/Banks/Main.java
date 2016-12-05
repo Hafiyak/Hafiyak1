c77b8ebddfb05996dabf0b7a6ab5e3f3a341b17f
Andriy Hafyak <x3x7x3@gmai.com>
2016-12-05 14:58:30 +0100

    Module 4

--- src/Banks/Main.java
+++ src/Banks/Main.java
@@ -4,14 +4,40 @@ package Banks;
  * Created by user on 04.12.2016.
  */
 public class Main {
+
     public static void main(String[] args) {
 
-        USBank usBank = new USBank();
-        USBank usBank1 = new USBank();
-        ChinaBank chinaBank = new ChinaBank();
-        ChinaBank chinaBank1 = new ChinaBank();
-        EUBank euBank = new EUBank();
-        EUBank euBank1 = new EUBank();
+        ChinaBank();
+        ChinaBank1();
+        EUBank();
+        EUBank1();
+        USBank();
+        USBank1();
+
+
+    }
+
+    static void ChinaBank(){
+
+    }
+
+    static void ChinaBank1(){
+
+    }
+
+    static void EUBank(){
+
+    }
+
+    static void EUBank1(){
+
+    }
+
+    static void USBank(){
+
+    }
+
+    static void USBank1(){
 
 
     }
